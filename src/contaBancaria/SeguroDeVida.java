37ee7214eb3b1e8d5c2a40b339058a53efcf605d
i03 <i03@l-1-10-11--27.satc.edu.br>
2014-07-26 11:27:18 -0300

    Inicio do projeto

---
+++ src/contaBancaria/SeguroDeVida.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package contaBancaria;
+
+/**
+ *
+ * @author i03
+ */
+class SeguroDeVida implements Tributavel{
+    public double calculaTributos(){
+        return 42;
+    }
+}
